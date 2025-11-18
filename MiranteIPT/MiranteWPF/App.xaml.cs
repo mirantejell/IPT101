5955c19aa25f53686bd82cc1b9e2337374273c15
mirantejell <143576336+mirantejell@users.noreply.github.com>
2025-11-18 18:25:45 +0800

    Add files via upload

---
+++ MiranteIPT/MiranteWPF/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiranteWPF.HostBuilders;
+
+namespace MiranteWPF;
+
+public partial class App : Application
+{
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .AddConfiguration()
+            .AddServices()
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
+}
