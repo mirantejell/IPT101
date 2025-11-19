e99d592e1a5496ef8b1df0ddabcc1565046e3752
mirantejell <143576336+mirantejell@users.noreply.github.com>
2025-11-19 11:08:36 +0800

    Add files via upload

---
+++ MiranteIPT/MiranteYoutubeViewer/Components/YouTubeViewersDetails.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiranteIPT101Solution.MiranteYoutubeViewer.Components
+{
+    /// <summary>
+    /// Interaction logic for YouTubeViewersDetails.xaml
+    /// </summary>
+    public partial class YouTubeViewersDetails : UserControl
+    {
+        public YouTubeViewersDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
