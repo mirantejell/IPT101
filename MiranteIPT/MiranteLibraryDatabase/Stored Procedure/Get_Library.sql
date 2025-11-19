26dcef30c7976fa58bcbcbce9bdc51bad49ce57c
mirantejell <143576336+mirantejell@users.noreply.github.com>
2025-11-19 10:55:40 +0800

    Add files via upload

---
+++ MiranteIPT/MiranteLibraryDatabase/Stored Procedure/Get_Library.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[Get_Library]
+	@BookId NVARCHAR(100) = NULL
+AS
+BEGIN
+	SELECT * FROM [dbo].[Books] AS a WHERE a.[BookId] = @BookId;
+END
---
+++ MiranteIPT/MiranteLibraryDatabase/Tables/Books.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+    BookID NVARCHAR(40),
+    Title NVARCHAR(100),
+    Author NVARCHAR(100),
+    Category NVARCHAR(50),
+    Availability NVARCHAR(10)
+);
